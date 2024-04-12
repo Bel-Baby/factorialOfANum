12295e98b3e57f27604838e6e727488fa70c090a
Abel Amuta <amutaprincedavid@gmail.com>
2024-04-12 14:11:06 +0100

    refactored arrayFrom to a reusable code

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 //write a program to calculate the factorial of a number.eg: factorial of 4 is 4*3*2*1 = 25
+    
 
+        // --Check factor using forLoops--
 // const checkBtn = document.getElementById("check");
 
 // const checkFactorial = () => {
@@ -22,40 +24,49 @@
 // checkBtn.addEventListener("click",checkFactorial);
 
 
-// const checkBtn = document.getElementById("check");
+        // --Check factor using ArrayFrom--
+
+//const checkBtn = document.getElementById("check");
 
 // const checkFactorial = () => {
 //     const input = document.getElementById("input");
 //     const result = document.getElementById("result");
 //     let factorial = input.value;
-//     let arr = Array.from({length:factorial-1},(_,i) => {
-//         i -= 1;
-//         factorial *= i;
-//         if (factorial == 0) {
-//             result.textContent = `0 = 0`;
-//         }else {
-//             result.textContent = `${input.value}x${arr.join("x")} = ${factorial}`;
-//         }
-//     });
+
+//     const arr = Array.from({ length: factorial - 1 }, (_, i) => i + 1);
+//     const resultFactorial = arr.reduce((acc, val) => acc * val, factorial);
+
+//     if (factorial == 0) {
+//         result.textContent = `${input.value} = ${resultFactorial}`;
+//     } else {
+//         result.textContent = `${input.value}x${arr.reverse().join("x")} = ${resultFactorial}`;
+//     }
 // }
 
-// checkBtn.addEventListener("click",checkFactorial);
+//checkBtn.addEventListener("click", checkFactorial);
 
+
+        // --Check factor using ArrayFrom(Reuseable format)--
 const checkBtn = document.getElementById("check");
 
-const checkFactorial = () => {
-    const input = document.getElementById("input");
-    const result = document.getElementById("result");
-    let factorial = input.value;
+const getFactorial = (factorial) => {
+    const factorialData = Array.from({ length: factorial - 1 }, (_, i) => i + 1);
+    const resultFactorial = factorialData.reduce((acc, val) => acc * val, factorial);
 
-    const arr = Array.from({ length: factorial - 1 }, (_, i) => i + 1);
-    const resultFactorial = arr.reduce((acc, val) => acc * val, factorial);
+    return { factorial: resultFactorial, factorialData }
+}
+
+const showFactor = () => {
+    const input = document.getElementById("input");
+    let factorial = input.value;
+    const result = document.getElementById("result");
+    const factors = getFactorial(factorial);
 
     if (factorial == 0) {
-        result.textContent = `${input.value} = ${resultFactorial}`;
+        result.innerHTML = `${input.value} = ${factors.factorial}`;
     } else {
-        result.textContent = `${input.value}x${arr.reverse().join("x")} = ${resultFactorial}`;
+        result.innerHTML = `${input.value}x${factors.factorialData.reverse().join("x")} = ${factors.factorial}`;
     }
 }
 
-checkBtn.addEventListener("click", checkFactorial);
+checkBtn.addEventListener("click", showFactor);
